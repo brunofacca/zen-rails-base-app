03ae634ff6fc007a025dffcfbca3785c6afccb02
Bruno Facca <bruno@facca.info>
2021-09-12 13:58:40 -0300

    Update config.load_defaults to 6.1

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ZenRailsBaseApp
   :nodoc
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified
     # here. Application configuration should go into files in
